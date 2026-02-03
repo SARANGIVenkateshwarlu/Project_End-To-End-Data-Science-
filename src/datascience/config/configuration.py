4178870333c241b9613a43c6246cd627d4d9c13f
Sarangi_Venkateshwarlu <venky.sarangi@gmail.com>
2026-02-04 00:21:50 +0800

    model_evaluation

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,7 +1,9 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
-from src.datascience.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
+from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,
+                                                  DataTransformationConfig,ModelTrainerConfig,
+                                                  ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -44,18 +46,14 @@ class ConfigurationManager:
         return data_validation_config
     
     def get_data_transformation_config(self) -> DataTransformationConfig:
-        config = self.config.data_transformation
-
+        config=self.config.data_transformation
         create_directories([config.root_dir])
-
-        data_transformation_config = DataTransformationConfig(
+        data_transformation_config=DataTransformationConfig(
             root_dir=config.root_dir,
-            data_path=config.data_path,
+            data_path=config.data_path
         )
-
         return data_transformation_config
     
-
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.ElasticNet
@@ -75,3 +73,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/venky.sarangi/Project_End-To-End-Data-Science-.mlflow"
+
+
+        )
+        return model_evaluation_config
